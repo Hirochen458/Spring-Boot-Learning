18951816f49c9bc8ef982613bc2b790d86a0590e
Hirochen458 <hirochen458@gmail.com>
2023-08-04 10:14:32 -0400

    fifth commit

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcodedemo/rest/DemoController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springcodedemo.rest;
+
+import com.luv2code.springcodedemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define a private field for dependency
+    private Coach myCoach;
+
+    //Define a constructor for dependency injection
+    @Autowired
+    public DemoController(@Qualifier("trackCoach") Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    //Setter injection
+//    @Autowired
+//    public void setCoach(Coach theCoach){
+//        myCoach = theCoach;
+//    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luv2code/springcodedemo/rest/DemoController.java
@@ -0,0 +1,42 @@
+package com.luv2code.springcodedemo.rest;
+
+import com.luv2code.springcodedemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //define a private field for dependency
+    private Coach myCoach;
+
+    //Define a constructor for dependency injection with Qualifier
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+
+        System.out.println("In constructor: " + getClass().getSimpleName());
+
+        myCoach = theCoach;
+    }
+
+
+    //Define a constructor for dependency injection with primary or not
+//    @Autowired
+//    public DemoController(Coach theCoach){
+//        myCoach = theCoach;
+//    }
+
+
+    //Setter injection
+//    @Autowired
+//    public void setCoach(Coach theCoach){
+//        myCoach = theCoach;
+//    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
