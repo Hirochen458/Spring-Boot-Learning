0dd7569afb9af13a15806ff99b3ef15fa66599c4
Hirochen458 <hirochen458@gmail.com>
2023-08-09 14:33:46 -0400

    REST DAO

--- 04-spring-boot-REST-CRUD/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDaoJpaImpl.java
+++ 04-spring-boot-REST-CRUD/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDaoJpaImpl.java
@@ -27,17 +27,43 @@ public class EmployeeDaoJpaImpl implements EmployeeDAO{
         //create a query
         TypedQuery<Employee> theQuery = entityManager.createQuery("from Employee", Employee.class);
 
+        //execute query and get result list
         List<Employee> employees = theQuery.getResultList();
 
-        //execute query and get result list
-
-
         //return the results
-
-
         return employees;
     }
 
+    @Override
+    public Employee findById(int theId) {
+
+        //get employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        //return employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+
+        //save employee
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        //return dbEmployee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+        //find employee by id
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        //remove employee
+        entityManager.remove(theEmployee);
+
+    }
 
 
 }
