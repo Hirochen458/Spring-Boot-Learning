24350fd9e13df0a0f33be0477bb3e6151d1ab3d5
Hirochen458 <hirochen458@gmail.com>
2023-08-16 18:31:07 -0400

    validation

--- 06-spring-boot-spring-MVC/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-MVC/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -3,7 +3,7 @@ package com.luv2code.springdemo.mvc;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public class Custmoer {
+public class Customer {
 
     private String firstName;
 
